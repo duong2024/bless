4c320cddafe8643f5927ee034a6077f76f7870b9
Dương <duong2024@gmail.com>
2024-11-19 12:42:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        usertoken: 'usertoken1',
+        nodes: [
+            { nodeId: '12D3KooWG7hFAJFust1vExi2mQzto9Q6xgbKpwKdhaoy5a5YoPsB', hardwareId: '8082bc37aa7b9bbc6664b36e47da9ac6b58a459a8c4c441864fe4cf100f4b613', proxy: 'http://ybiyudei:dtzm0sruse7f@171.22.250.22:6141' }
+        ]
+    }
+    // Add more usertokens as needed
+];
